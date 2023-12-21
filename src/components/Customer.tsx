36d6ea61b9e23005d4b5ed22ef421d2003574ab9
salmanMohammed2001 <salmannisthar123@gmail.com>
2023-12-21 12:34:15 +0530

    customerUi initialize

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -2,7 +2,87 @@
 function Customer(){
     return(
         <div>
-            <h1>Customer</h1>
+            <br/>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-sm-6 col-md-4">
+                            <div className="form-group">
+                                <label htmlFor="customername">Customer Name</label>
+                                <input type="text" className="form-control" id='customerName'/>
+                            </div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <div className="form-group">
+                            <label htmlFor="customerAddress">Customer Address</label>
+                            <input type="text" className="form-control" id='customerAddes'/>
+                        </div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <div className="form-group">
+                            <label htmlFor="customerSalary">Customer Salary</label>
+                            <input type="text" className="form-control" id='customerSalary'/>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                    <div className="row">
+                        <div className="col-12">
+                            <button className='btn btn-primary col-12'>Save Customer</button>
+                        </div>
+                    </div>
+                <hr/>
+                <div className="row">
+                    <div className="col-12">
+                        <form action="" className='col-12'>
+                            <input type="search" className='form-control' placeholder='Search Customer'/>
+                        </form>
+                    </div>
+                </div>
+                <br/>
+                <div className="row">
+                    <div className="col-12">
+                        <table className='table table-hover table-bordered'>
+                            <thead>
+                            <tr>
+                                <th>#Id</th>
+                                <th>Name</th>
+                                <th>Address</th>
+                                <th>Salary</th>
+                                <th>Delete</th>
+                                <th>Update</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>#1001</td>
+                                <td>nimal</td>
+                                <td>colombo</td>
+                                <td>5000.00</td>
+                                <td>
+                                    <button className='btn btn-outline-danger btn-sm'>Delete</button>
+                                </td>
+                                <td>
+                                    <button className='btn btn-outline-success btn-sm'>Update</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>#1001</td>
+                                <td>nimal</td>
+                                <td>colombo</td>
+                                <td>5000.00</td>
+                                <td>
+                                    <button className='btn btn-outline-danger btn-sm'>Delete</button>
+                                </td>
+                                <td>
+                                    <button className='btn btn-outline-success btn-sm'>Update</button>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                </div>
         </div>
     )
 
