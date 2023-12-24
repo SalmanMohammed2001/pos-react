03066e6bb59d5d66b653ae7b66340ad1c983ab50
salmanMohammed2001 <salmannisthar123@gmail.com>
2023-12-24 22:29:36 +0530

    crete customer useState

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -1,5 +1,21 @@
+import React, {useState} from "react";
+interface Customer{
+    id:string,
+    name:string,
+    address:string,
+    salary:number
+}
 
-function Customer(){
+const Customer:React.FC=()=>{
+        const[name,setName]=useState('')
+        const[address,setAddress]=useState('')
+        const[salary,setSalary]=useState<number | "">('')
+
+
+    const saveCustomer=async ()=>{
+        console.log(name,address,salary)
+    }
+
     return(
         <div>
             <br/>
@@ -9,26 +25,26 @@ function Customer(){
                     <div className="col-12 col-sm-6 col-md-4">
                             <div className="form-group">
                                 <label htmlFor="customername">Customer Name</label>
-                                <input type="text" className="form-control" id='customerName'/>
+                                <input type="text" className="form-control" id='customerName' onChange={(e)=>{setName(e.target.value)}}/>
                             </div>
                     </div>
                     <div className="col-12 col-sm-6 col-md-4">
                         <div className="form-group">
                             <label htmlFor="customerAddress">Customer Address</label>
-                            <input type="text" className="form-control" id='customerAddes'/>
+                            <input type="text" className="form-control" id='customerAddes'onChange={(e)=>{setAddress(e.target.value)}}/>
                         </div>
                     </div>
                     <div className="col-12 col-sm-6 col-md-4">
                         <div className="form-group">
                             <label htmlFor="customerSalary">Customer Salary</label>
-                            <input type="text" className="form-control" id='customerSalary'/>
+                            <input type="text" className="form-control" id='customerSalary' onChange={(e)=>{setSalary(e.target.value==''?'':parseFloat(e.target.value))}}/>
                         </div>
                     </div>
                 </div>
                 <br/>
                     <div className="row">
                         <div className="col-12">
-                            <button className='btn btn-primary col-12'>Save Customer</button>
+                            <button className='btn btn-primary col-12' onClick={saveCustomer}>Save Customer</button>
                         </div>
                     </div>
                 <hr/>
